f1ccd83e6bcf48f91b0c1a28e8b16afb650b3f42
Bram <bramgriffioen98@gmail.com>
2022-01-26 22:34:49 +0100

    yes

--- datascience_practical.R
+++ datascience_practical.R
@@ -13,7 +13,9 @@
 # install.packages("regclass")
 # install.packages("dplyr")
 # install.packages("ggpubr")
-install.packages("SmartEDA")
+# install.packages("SmartEDA")
+# install.packages("jtools")
+# install.packages("stargazer")
 
 ##### Loading Libraries ##### 
 
@@ -27,6 +29,8 @@ library(flexsurv)
 library(reldist)
 library(dplyr)
 library(regclass)
+library(jtools)
+library(stargazer)
 
 ##### Loading Data ##### 
 
@@ -41,17 +45,18 @@ quantVariables = data1[,2:10]
 logExpenditureAB = data1[,8:9]
 expendAB = data1[,5:6]
 
+
 ##### Question 1 ######
 
 ### Expenditure A
 votingExpenditure.lm = lm(formula = voteA ~ expendA, data = data1)
-summary(votingExpenditure.lm)
+summ(votingExpenditure.lm)
 votingExpenditure.data = data.frame(data1$voteA, data1$expendA)
 pairs(votingExpenditure.data, panel=panel.smooth)
 
 ### Expenditure A/ Expenditure B
 expenditureAExpenditureB.lm = lm(expendA~ expendB, data = data1)
-summary(expenditureAExpenditureB.lm)
+#summary(expenditureAExpenditureB.lm)
 plot(data1$expendA, data1$expendB, xlab = 'Expenditure A', ylab = 'Expenditure B')
 
 ### Relative Expenditure A
@@ -60,39 +65,53 @@ summary(votingShareA.lm)
 votingShareA.data = data.frame(data1$voteA, data1$shareA)
 pairs(votingShareA.data, panel=panel.smooth)
 
-### Colliniearity
+### Colliniearity 
 votingExpenditureShareA.lm = lm(formula = voteA ~ shareA + expendA, data = data1)
 summary(votingExpenditureShareA.lm)
 VIF(votingExpenditureShareA.lm)
 votingExpenditureShareA.data = data.frame(data1$voteA, data1$shareA, data1$expendA)
 
 cor.data = quantVariables
-cor.cor = cor(cor.data)
+cor.cor = round(cor(cor.data),2)
+stargazer(cor.cor, digits=1, type='text', title = 'Correlation Matrix')
+
+
+
 
 ##### Question 2 ##### 
 
 ### Log vs Normal 
 votingLog.lm = lm(formula = voteA ~ lexpendA, data= data1)
-summary(votingLog)
-
-data1$l2expendA = exp(data1$expendA)
-votingExpendAExp = lm(formula = voteA ~ l2expendA, data=data1)
-summary(votingExpenditure.lm)
+votingExpendA.lm = lm(formula = voteA ~ expendA, data=data1)
 
 grid1 = matrix(c(1,2), nrow =1, ncol = 2)
 layout(grid1)
 plot(data1$lexpendA, data1$voteA, main = "Log Expenditure A vs Voting % A")
 plot(data1$expendA, data1$voteA,  main = "Expenditure A vs Voting % A")
 
-
 ##### LogA, LogB --- Voting A ##### 
 ## Log: 
-voteALogALogB.lm = lm(formula = data1$voteA ~ log$lexpendA + log$lexpendB)
-summary(voteALogALogB.lm)
+voteALogALogB.lm = lm(formula = data1$voteA ~ logExpenditureAB$lexpendA + logExpenditureAB$lexpendB)
 
 ## "Normal" 
-voteAExpAExpB.lm = lm(formula = data1$voteA ~ expend$expendA + expend$expendB)
-summary(voteAExpAExpB.lm)
+voteAExpAExpB.lm = lm(formula = data1$voteA ~ expendAB$expendA + expendAB$expendB)
+
+
+#### All variables ####
+colInterest = data1[, c('district', 'democA', 'voteA', 'expendA', 'expendB', 'prtystrA', 'lexpendA', 'lexpendB', 'shareA')]
+all.lm = lm(formula = data1$voteA ~ ., data= colInterest )
+narrower.data = data1[, c('prtystrA', 'shareA')]
+narrower.lm = lm(formula = data1$voteA ~ ., data= narrower.data )
+
+
+stargazer(all.lm, narrower.lm, votingShareA.lm,
+           type = "text", out = "fit_lm.html", omit.stat=c("LL","ser","f"), 
+           align=TRUE ,no.space=TRUE)
+
+stargazer(votingLog.lm, voteALogALogB.lm, votingExpend.lm, voteAExpAExpB.lm, votingShareA.lm,
+          type = "text", out = "fit_lm.html", omit.stat=c("LL","ser","f"), 
+          align=TRUE ,no.space=TRUE)
+
 
 
 ##### Interaction Term ##### 
@@ -105,10 +124,11 @@ summary(votingAInteractionTerm.lm)
 
 interactionTermLog = data1$lexpendA * data1$lexpendB
 logExpenditureAB$interactionTermlog = interactionTermLog
-logExpenditureAB
 votingAInteractionTermLog.lm = lm(formula = data1$voteA ~ logExpenditureAB$lexpendA + logExpenditureAB$lexpendB + logExpenditureAB$interactionTermlog)
-summary(votingAInteractionTermLog.lm)
 
+stargazer(votingAInteractionTermLog.lm, votingAInteractionTerm.lm,
+          type = "text", out = "fit_lm.html", omit.stat=c("LL","ser","f"), 
+          align=TRUE ,no.space=TRUE)
 
 #### 
 
